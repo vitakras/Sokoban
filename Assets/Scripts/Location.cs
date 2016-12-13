ccfcc37da0809c415b8ec288d49cdec0ed5dafb4
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-12-12 21:21:52 -0500

    Created location script

---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Location : MonoBehaviour {
+
+	public bool isOccupied = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log(collider.name);
+		this.isOccupied = true;
+	}
+
+	void OnTriggerExit2D(Collider2D collider) {
+		this.isOccupied = false;
+	}
+}
