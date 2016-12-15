43b229beffea6cab59b2963379ca9bd5d502a94c
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-12-14 21:45:08 -0500

    Updated Location script to update acordingly when a Box Object Enters and Exists

--- Assets/Scripts/Location.cs
+++ Assets/Scripts/Location.cs
@@ -4,23 +4,47 @@ using System.Collections;
 public class Location : MonoBehaviour {
 
 	public bool isOccupied = false;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public string ignoreTag = "Player";
 
 	void OnTriggerEnter2D(Collider2D collider) {
-		Debug.Log(collider.name);
-		this.isOccupied = true;
+		if (!collider.CompareTag(this.ignoreTag)) {
+			StartCoroutine(this.SetOccupied(collider));
+		} else {
+			Debug.Log("Player Entered Location");
+		}
 	}
 
 	void OnTriggerExit2D(Collider2D collider) {
-		this.isOccupied = false;
+		if (!collider.CompareTag(this.ignoreTag)) {
+			this.isOccupied = false;
+		} else {
+			Debug.Log("Ingoring Player");
+		}
+	}
+
+	public bool IsOccupied(){
+		return this.isOccupied;
+	}
+		
+	private IEnumerator SetOccupied(Collider2D collider) {
+		Movable movable = collider.GetComponent<Movable>();
+		Vector2 myPosition = this.gameObject.transform.position;
+		Vector2 movablePosition;
+
+		if (movable != null) {
+			while(movable.IsMoving()) {
+				movablePosition = collider.transform.position;
+				if (myPosition.Equals(movablePosition)) {
+					break;
+				}
+
+				yield return null;
+			}
+
+			movablePosition = collider.transform.position;
+			if (myPosition.Equals(movablePosition)) {
+				this.isOccupied = true;
+			}
+		}
 	}
 }
