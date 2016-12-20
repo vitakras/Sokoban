80ac7ba7863876a6f57bc6e4f93bce95fd2a4d08
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-12-19 20:54:31 -0500

    Updated LocationManager to Respond to Events and Trigger Events based on Locations

--- Assets/Scripts/LocationManager.cs
+++ Assets/Scripts/LocationManager.cs
@@ -1,25 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LocationManager : MonoBehaviour {
+public class LocationManager : MonoBehaviour
+{
 
-	private Location [] locations;
+	private Location[] locations;
+	private int occupiedCount;
 
 	// Use this for initialization
-	void Start () {
-		locations = FindObjectsOfType<Location>();
+	void Start ()
+	{
+		this.locations = FindObjectsOfType<Location> ();
+		this.ResetCount ();
 	}
 
-	public int Occupied() {
+	void OnEnable ()
+	{
+		EventManager eventManager = EventManager.intance;
+
+		if (eventManager) {
+			eventManager.RegisterAction ((int)EventType.LOCATION_OCCUPIED, this.UpdateOccupied);
+		}
+	}
+
+	void OnDisable ()
+	{
+		EventManager eventManager = EventManager.intance;
+
+		if (eventManager) {
+			eventManager.DeRegisterAction ((int)EventType.LOCATION_OCCUPIED, this.UpdateOccupied);
+		}
+	}
+
+	void ResetCount ()
+	{
+		this.occupiedCount = 0;
+	}
+
+	public void UpdateOccupied ()
+	{
 		int count = 0;
 
-		foreach(Location location in this.locations){
-			if (location.IsOccupied){
+		foreach (Location location in this.locations) {
+			if (location.IsOccupied) {
 				count++;
 			}
 		}
 
-		return count;
+		this.occupiedCount = count;
+
+		if (this.OccupiedCount == this.Count) {
+			this.AllLocationsOccupied ();
+		}
 	}
 
+	public int Count {
+		get {
+			if (this.locations != null) {
+				return this.locations.Length;
+			} else {
+				return 0;
+			}
+		}
+	}
+
+	public int OccupiedCount {
+		get {
+			return this.occupiedCount;
+		}
+	}
+
+	private void AllLocationsOccupied ()
+	{
+		EventManager eventManager = EventManager.intance;
+
+		if (eventManager) {
+			eventManager.TriggerEvent ((int)EventType.ALL_LOCATIONS_OCCUPIED);
+		}
+	}
 }
